51638f8f94d781d629eaea1dca87b8ff524ce927
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2023-06-11 22:40:19 -0500

    Falta hacer la biblioteca

--- Tercer Parcial/Proyecto4/boton4.asm
+++ Tercer Parcial/Proyecto4/boton4.asm
@@ -7,20 +7,33 @@ INCLUDE MACRO.LIB
     
     MSG DB "SALIR $"
     
-    ALTURA DB 0
-    ANCHO DB 0
-    INICIOC DB 0
-    INICIOR DB 0
-    FINC DB 0
-    FINR DB 0
+    ;DATOS DEL RECT?NGULO 
+    ALTURA DW 0 
+    ANCHO DW 0
+    INICIOC DW 0
+    INICIOR DW 0
+    FINC DW 0
+    FINR DW 0
     COLOR DB 05H
      
+    ;VARIABLE PARA EL MODO GRAFICO
     MODE DB ? 
+    
+    ;TIEMPO DE LOS CLICKS
     TIEMPO1 DW 0
     TIEMPO2 DW 0
     
-
-    ENMASCARAR_PUNTERO   dw 1111110000111111b 
+    ;COORDENADAS DEL CLICK
+    X1 DW 0 ;HORIZONTAL
+    Y1 DW 0 ;VERTICAL
+    X2 DW 0
+    Y2 DW 0
+    
+    ;BANDERAS DE VALIDACION 
+    B1 DB 0
+    B2 DB 0
+    
+    ENMASCARAR_PUNTERO   dw 1111110000111111b  
             dw 1111110000111111b              
             dw 1111110000111111b
             dw 1111110000111111b
@@ -36,30 +49,46 @@ INCLUDE MACRO.LIB
             dw 1111110000111111b
             dw 1111110000111111b
             dw 1111110000111111b
-            
+            ;TRANSPARENTE(0) O NO(1)
+            dw 0000011111100000b 
+            dw 0000010000100000b              
+            dw 0000010000100000b
+            dw 0000010000100000b
+            dw 0000010000100000b
+            dw 1111110000111111b
+            dw 1000000000000001b
+            dw 1000000000000001b
+            dw 1000000000000001b
+            dw 1000000000000001b
+            dw 1111110000111111b              
+            dw 0000010000100000b
+            dw 0000010000100000b
+            dw 0000010000100000b
+            dw 0000010000100000b
+            dw 0000011111100000b            
 .CODE
 MAIN PROC
     INICIO_DC
     ;procedemos a trabajar en pixeles
     MOV ES,AX
-    mov MODE, 12h 
+    MOV MODE, 12h 
     CALL MODOVIDEO
     
-    MOV ANCHO, 150 ; Ancho del rect?ngulo
-    MOV ALTURA, 100 ; Altura del rect?ngulo
+    MOV ANCHO, 150 ; Ancho del rectangulo
+    MOV ALTURA, 100 ; Altura del rectangulo
     
     MOV INICIOC,64H ;COLUMNA DE INICIA
     MOV INICIOR,32H ;RENGLON DE INICIO
     
-    MOV AH,INICIOC
-    MOV FINC,AH
-    MOV AH,ANCHO
-    ADD FINC,AH
+    MOV AX,INICIOC
+    MOV FINC,AX
+    MOV AX,ANCHO
+    ADD FINC,AX
     
-    MOV AL,INICIOR
-    MOV FINR,AL
-    MOV AL,ALTURA
-    ADD FINR,AL
+    MOV AX,INICIOR
+    MOV FINR,AX
+    MOV AX,ALTURA
+    ADD FINR,AX
     
     CALL DRAW_RECTANGLE
     
@@ -69,11 +98,33 @@ MAIN PROC
 
     POSICIONA_CURSOR 6,19
     ESCRIBE_CADENA MSG
-
-    CALL DOBLE_CLICK
     
+    OTRA_VEZ:
+    MOV AX,0
+    MOV BX,0
+    MOV CX,0
+    MOV DX,0
+    CALL DOBLE_CLICK
+    CALL RASTREAR_COORDENADA
+    CMP B2,0
+    ;JE ESPERA
+    JE OTRA_VEZ
     CALL ENMASCARAR_CURSOR 
     M_MOUSE
+    JMP FIN_PROGRAMA
+    
+    ESPERA:
+      WAIT_CLICK:
+        MOV AX,02H ;Read Real-Time Clock Time
+        INT 1AH    ;DX tiene segundos(DH) y microsegundos (DL)
+        
+        SUB DX,TIEMPO1
+        CMP DL,03H             ;Ignoramos los segundo y hablamos en terminos de microsegundos
+        JB WAIT_CLICK   ; Si no ha pasado el tiempo suficiente, esperar mas
+       JMP OTRA_VEZ
+       
+       
+    FIN_PROGRAMA:
     POSICIONA_CURSOR 20,5
     EXIT_PROGRAMA
     RET
@@ -84,21 +135,19 @@ DRAW_RECTANGLE PROC
    PUSH BX
    PUSH CX
    PUSH DX
-   
-        MOV DH,0
-        MOV DL,INICIOR
+           
+        MOV DX,INICIOR
     FILA:
-        MOV CH,0
-        MOV CL,INICIOC
+        MOV CX,INICIOC
         
        OTRAC:
         CALL PIXEL
-        INC CL
-        CMP CL,FINC
+        INC CX
+        CMP CX,FINC
         JNE OTRAC
         
-        INC DL
-        CMP DL,FINR
+        INC DX
+        CMP DX,FINR
         JNE FILA
         
         
@@ -124,12 +173,10 @@ RET
 PIXEL ENDP
 
 MODOVIDEO PROC
-
     ; modo de video 
-    mov al, MODE 
-    mov ah, 0 
-    int 10h 
-
+    MOV AL,MODE
+    MOV AH,0
+    INT 10H
 RET
 MODOVIDEO ENDP
 
@@ -137,38 +184,57 @@ DOBLE_CLICK PROC NEAR
 SAL3:
     MOV AX, 03h ; Obtener estado de los botones del mouse
     INT 33h
-    CMP BX, 01h ; Bot?n izquierdo presionado
+    CMP BX, 01h ; Boton izquierdo presionado
     JNE SAL3
-
-    ;GUARDAMOS INFO DEL CLICK
-        MOV AX, 02H ; Inicializar temporizador del sistema
-        INT 1Ah ; Leer el temporizador del sistema en CX:DX
-        MOV tiempo1, DX ; Guardar el valor del temporizador del primer clic
+    ;GUARDAMOS COORDENADAS
+    MOV X1,CX
+    MOV Y1,DX
+    ;GUARDAMOS INFO DEL PRIMER CLICK
+        MOV AX, 02H     ; Read Real-Time Clock Time
+        INT 1Ah 
+        MOV TIEMPO1, DX ; Guardar el valor del temporizador del primer click
     
-
+    
     WAIT_SECOND_CLICK:
-        MOV AX,02H
-        INT 1AH
+        MOV AX,02H ;Read Real-Time Clock Time
+        INT 1AH    ;DX tiene segundos(DH) y microsegundos (DL)
         
         SUB DX,TIEMPO1
         CMP DL,03H             ;Ignoramos los segundo y hablamos en terminos de microsegundos
-        JB WAIT_SECOND_CLICK ; Si no ha pasado el tiempo suficiente, esperar mas
+        JB WAIT_SECOND_CLICK   ; Si no ha pasado el tiempo suficiente, esperar mas
 
         
-        ;segundo click
+        ;Segundo click
         MOV AX, 03h ; Obtener estado de los botones del mouse
         INT 33h
-        CMP BX, 01h ; Bot?n izquierdo presionado
+        CMP BX, 01h ; Boton izquierdo presionado
         JNE SAL3
-        MOV AX, 0 ; Reiniciar temporizador del sistema
-        INT 1Ah ; Leer el temporizador del sistema en DX:AX
+        
+        ;GUARDAMOS COORDENADAS
+        MOV X2,CX
+        MOV Y2,DX
+        ;COMPARAMOS 
+        MOV AX,X1
+        MOV BX,X2
+        CMP AX,BX
+        JNE SAL3
+        MOV AX,Y1
+        MOV BX,Y2
+        CMP AX,BX
+        JNE SAL3
+        
+        ;GUARDAMOS INFO DEL SEGUNDO CLICK
+        MOV AX, 02H ; Read Real-Time Clock Time
+        INT 1Ah 
+        MOV TIEMPO2, DX ; Guardar el valor del temporizador del segundo click
+        
         
         MOV AX,TIEMPO1
         MOV DX,TIEMPO2
         CMP AX,DX
         JE SAL3
         ; Si llegamos a este punto, se ha detectado un doble clic
-        ESCRIBE_CADENA MSG ;Letrero para probar
+        
         JMP SAL1
 
 SAL1:
@@ -176,13 +242,62 @@ SAL1:
 DOBLE_CLICK ENDP
 
 ENMASCARAR_CURSOR PROC
-    mov ax,09h ;con la forma personalizada 
-    xor bx,bx
-    xor cx,cx
-    lea dx,enmascarar_puntero
+    MOV AX,09H ;Colocar un cursor personalizado
+    XOR BX,BX
+    XOR CX,CX
+    LEA DX,ENMASCARAR_PUNTERO
     INT 33H
-    ret
+    RET
 ENMASCARAR_CURSOR ENDP
 
+RASTREAR_COORDENADA PROC
+    ;BASTA CON EVALUAR UNA DE LAS DOS PUES YA SE VALIDO SON IGUALES
+    
+
+    ;MOV AX,X1
+    ;DESEMPAQUETA AH
+    ;DESEMPAQUETA AL
+    ;MOV AX,Y1
+    ;DESEMPAQUETA AH
+    ;DESEMPAQUETA AL
+    
+    MOV AX,X1
+    ;DESEMPAQUETA AH
+    ;DESEMPAQUETA AL
+    
+    MOV CX,INICIOC
+    ;DESEMPAQUETA CH
+    ;DESEMPAQUETA CL
+    
+    COL:
+        CMP AX,CX
+        JE BANDERA1
+        INC CX
+        CMP CX,FINC
+        JNE COL
+ 
+        JMP FIN_R ;NO COINCIDIO, FINALIZAMOS DE UNA VEZ
+      
+     VALIDA2:
+        MOV AX,Y1
+        MOV CX,INICIOR
+        REN:
+            CMP AX,CX
+            JE BANDERA2
+            INC CX
+            CMP CX,FINR
+            JNE REN
+        JMP FIN_R
+        
+     BANDERA1:
+        MOV B1,1
+        JMP VALIDA2
+        
+     BANDERA2:
+        MOV B2,1
+    
+  FIN_R:
+    RET
+RASTREAR_COORDENADA ENDP
 
 END MAIN
